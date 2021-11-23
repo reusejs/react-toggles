2d528dbf2f1bdc91d96c182eb05b4b12a876f8c3
Rajiv Seelam <rajiv@betalectic.com>
2021-11-23 22:30:24 +0530

    Add commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import postcss from "rollup-plugin-postcss";
+import commonjs from "rollup-plugin-commonjs";
 
 export default {
   input: ["index.js", "src/Base/index.js"],
